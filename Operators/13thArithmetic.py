0403289e8666267016cb3a783fe5e83d667c8b56
BALU <akkinibalu@gmail.com>
2023-10-14 22:43:30 -0400

    code used print formatted

---
+++ Operators/13thArithmetic.py
@@ -0,0 +1,25 @@
+a = int(input("Enter a value \n"))
+b = int(input("Enter b Value \n"))
+
+print("Sum is = ",a+b)
+print("Sub is =",a-b)
+print("Multiplication is = ",a*b)
+print("modulo is = ",a%b)
+print("divide is = ",a/b)
+print("exponential is = ",a**b)
+print("floor division is= ",a//b)
+# Write a Python program to calculate the area of a circle given its radius using the formula
+# area=π×r^2 ( Take pie as 3.14)
+r = float(input("Enter the radius vaiue \n"))
+area = float(3.14*r*r)
+print("Area of the circle is:",area)
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+# Use the ternary operator to find the maximum of three numbers entered by the user.
+# Develop a Python script that calculates the square and cube of a given number.
+
+
+
+
